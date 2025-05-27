5af2c9805b1484e1f93bbe21193293d0feb07edf
Nguyntuanvu1203 <nguyntuanvu12@gmail.com>
2025-05-27 09:59:18 +0700

    Sua doi AddContactForm

--- ContactList/AddContactForm.Designer.cs
+++ ContactList/AddContactForm.Designer.cs
@@ -35,6 +35,7 @@
             this.btnSave = new System.Windows.Forms.Button();
             this.txtMail = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -96,11 +97,21 @@
             this.label3.TabIndex = 5;
             this.label3.Text = "Mail";
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(260, 380);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(113, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Thông tin bổ sung";
+            // 
             // AddContactForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.txtMail);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.btnSave);
@@ -124,5 +135,6 @@
         private System.Windows.Forms.Button btnSave;
         private System.Windows.Forms.TextBox txtMail;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
